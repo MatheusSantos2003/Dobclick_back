0d7458edeffe2752a250774e3c8e6c69c2152a2e
matheus santos <mt.pesc@hotmail.com>
2022-05-09 15:33:17 -0300

    correção de deploy

--- src/datasource.ts
+++ src/datasource.ts
@@ -1,4 +1,9 @@
 import { DataSource } from "typeorm";
+import CategoriaEntity from "./entity/Categoria";
+import { EstoqueEntity } from "./entity/Estoque";
+import { MarcaEntity } from "./entity/Marca";
+import { ProdutoEntity } from "./entity/Produto";
+import { UsuarioEntity } from "./entity/Usuario";
 
 
 require("dotenv").config();
@@ -12,7 +17,7 @@ export const AppDataSource = new DataSource({
   database: process.env.DATABASE_SCHEMANAME || "dobclick",
   synchronize: true,
   logging: false,
-  entities: ["src/entity/*.ts"],
+  entities: [CategoriaEntity,EstoqueEntity,MarcaEntity,ProdutoEntity,UsuarioEntity],
   migrations: ["src/migration/**/*.ts"],
   subscribers: ["src/subscriber/**/*.ts"],
 });
