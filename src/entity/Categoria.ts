3533cbdd66561b38884d84ca5b2f90837926954a
matheus santos <mt.pesc@hotmail.com>
2022-05-09 15:29:16 -0300

    correção de deploy

--- src/entity/Categoria.ts
+++ src/entity/Categoria.ts
@@ -1,9 +1,9 @@
 import { Column, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from "typeorm";
-import { ProdutoEntity } from "./Produto";
+
 
 
 @Entity({ name: "Categoria" })
-export class CategoriaEntity {
+export default class CategoriaEntity {
 
     @PrimaryGeneratedColumn()
     Id?: number;
