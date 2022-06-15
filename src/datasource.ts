01ec4212c857c7901b04d40d92fef3b114de58e0
MatheusHenrique200302 <mt.pesc@hotmail.com>
2022-06-15 22:42:42 +0000

    correção de datasource

--- src/datasource.ts
+++ src/datasource.ts
@@ -12,11 +12,11 @@ require("dotenv").config();
 
 export const AppDataSource = new DataSource({
   type: "postgres",
-  host: "localhost",
+  host:  process.env.DATABASE_URL || "localhost",
   port: 5432,
-  username:  "postgres",
-  password:  "root",
-  database:  "dobclick",
+  username: process.env.DATABASE_USER || "postgres",
+  password: process.env.DATABASE_PWD || "root",
+  database: process.env.DATABASE_SCHEMANAME || "dobclick",
   synchronize: true,
   logging: false,
   entities: ["src/entity/**/*.ts"],
